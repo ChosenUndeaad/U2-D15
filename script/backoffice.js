de48c205b1486c73b11ec02390806ec4d9b921e6
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-03-21 11:25:21 +0100

    Daily exercise

--- script/backoffice.js
+++ script/backoffice.js
@@ -1,4 +1,46 @@
-const apiKey =
-  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-    .eyJfaWQiOiI2N2RkMmI3NzM4MzRiZjAwMTUwMDA3MDMiLCJpYXQiOjE3NDI1NDc4MzEsImV4cCI6MTc0Mzc1NzQzMX0
-    .Q4xSc4mZgUIHI4V4U51iyFFW96LvAkLylYjDRlTDPvc;
+//Funzione per la data nel footer
+const dateFooter = function () {
+  const footerSpan = document.getElementById("year");
+  footerSpan.innerText = new Date().getFullYear();
+};
+
+dateFooter();
+
+class shopItem {
+  constructor(_name, _description, _monster, _imageUrl, _price) {
+    this.name = _name;
+    this.description = _description;
+    this.monster = _monster;
+    this.imageUrl = _imageUrl;
+    this.price = _price;
+  }
+}
+
+const nameInpupt = document.getElementById("name");
+const descriptionInpupt = document.getElementById("description");
+const monsterInpupt = document.getElementById("monster");
+const imageUrlInpupt = document.getElementById("imageUrl");
+const priceInpupt = document.getElementById("price");
+
+const productUrl = "https://striveschool-api.herokuapp.com/api/product/";
+
+if (itemID) {
+  fetch(productUrl + "/" + itemID)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Errore nel fetch");
+      }
+    })
+    .then((data) => {
+      nameInput.value = data.name;
+      descriptionInput.value = data.description;
+      monsterInput.value = data.monster;
+      imageUrlInput.value = data.imageUrl;
+      priceInput.value = data.price;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
--- script/homepage.js
+++ script/homepage.js
@@ -1,7 +1,7 @@
-const apiKey =
-  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-    .eyJfaWQiOiI2N2RkMmI3NzM4MzRiZjAwMTUwMDA3MDMiLCJpYXQiOjE3NDI1NDc4MzEsImV4cCI6MTc0Mzc1NzQzMX0
-    .Q4xSc4mZgUIHI4V4U51iyFFW96LvAkLylYjDRlTDPvc;
+//const apiKey =
+//  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+//    .eyJfaWQiOiI2N2RkMmI3NzM4MzRiZjAwMTUwMDA3MDMiLCJpYXQiOjE3NDI1NTAzNzAsImV4cCI6MTc0Mzc1OTk3MH0
+//    .L2mCSVC6ZBiXR0V4uPrNvBWyqjF9l_1vAOFJYx8TCYE;
 
 //Funzione per la data nel footer
 const dateFooter = function () {
@@ -13,9 +13,40 @@ dateFooter();
 
 //
 const getShopItems = function () {
-  fetch("https://striveschool-api.herokuapp.com/api/put-your-endpoint-here/", {
+  fetch("https://striveschool-api.herokuapp.com/api/product/", {
     headers: {
-      Authorization: `Bearer ${apiKey}`,
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2RkMmI3NzM4MzRiZjAwMTUwMDA3MDMiLCJpYXQiOjE3NDI1NDc4MzEsImV4cCI6MTc0Mzc1NzQzMX0.Q4xSc4mZgUIHI4V4U51iyFFW96LvAkLylYjDRlTDPvc",
     },
-  });
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Error: Failed to fetch data");
+      }
+    })
+    .then((data) => {
+      console.log("Dati ricevuti:", data);
+
+      const row = document.getElementById("shop-cards");
+      // Loop per le card
+      data.forEach((item) => {
+        row.innerHTML =
+          row.innerHTML +
+          `<div class="card" style="width: 18rem;">
+              <img src="${item.imageUrl}" class="card-img-top" alt="${item.name}">
+               <div class="card-body">
+                  <h5 class="card-title">${item.name}</h5>
+                  <p class="card-text">${item.description}</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+          </div>`;
+      });
+    })
+    .catch((error) => {
+      console.error("ERRORE:", error);
+    });
 };
+
+getShopItems();
